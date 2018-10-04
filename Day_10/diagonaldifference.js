c183084ea9d818842d8e2f87a89bb240988c9f36
Ahmed Kashif Hussain <ahmedkashif.h@techaspect.com>
2018-10-04 11:50:27 +0530

    Added Source Code for Challange Diagonal Difference

---
+++ Day_10/diagonaldifference.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the diagonalDifference function below.
+function diagonalDifference(arr) {
+
+    var d1 = 0;
+    var d2 = 0;
+    var len = arr.length;
+    for( var i = 0; i < len; i++ ) {
+        d1 += arr[i][i];
+        d2 += arr[(len-1) - i][i];
+        
+    }
+    return Math.abs(d1 - d2);
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    let arr = Array(n);
+
+    for (let i = 0; i < n; i++) {
+        arr[i] = readLine().split(' ').map(arrTemp => parseInt(arrTemp, 10));
+    }
+
+    const result = diagonalDifference(arr);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
